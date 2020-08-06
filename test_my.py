c8549b4320bdfa0deed3e0497aa08e4b57a36a9a
ScreamingOnMee <lijian@boco.com.cn>
2020-08-06 16:43:02 +0800

    改为计算utf8下字符串字节数

--- test_my.py
+++ test_my.py
@@ -1,5 +1,2 @@
-import os
-
-a = os.getcwd()
-print(a)
-# a
+contents = "disk.fileexistandsize[/opt/mqadmin/FmsNpiDataLoader/tongfang/FMS-FM_SUM_1440-0-'(date -d '-4 day' +%Y%m%d)'000000-'(date -d '-4 day' +%Y%m%d)'000000-001.zip]"
+print(len(contents.encode()))
